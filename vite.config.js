dfcad64b0da14a7dc056d4665e2827e895b601c6
Eduysuscosas <123275301+Eduysuscosas@users.noreply.github.com>
2025-07-06 10:14:29 +0200

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/alcala-tourism-pwa/' : '/',
+  base: '/alcala-tourism-pwa/', // Quita la condición de NODE_ENV temporalmente
 })
